cf7e8481f95ccd71c86df76f5aed71c1810d68c0
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-05-24 22:27:39 +0900

    revise native code

--- modules/sql/src/main/java/com/nautilus_technologies/tsubakuro/impl/low/sql/LinkImpl.java
+++ modules/sql/src/main/java/com/nautilus_technologies/tsubakuro/impl/low/sql/LinkImpl.java
@@ -1,11 +1,33 @@
 package com.nautilus_technologies.tsubakuro.impl.low.sql;
 
 import java.nio.ByteBuffer;
+import java.io.Closeable;
+import java.io.IOException;
 
 /**
  * SessionLinkImpl type.
  */
-public class LinkImpl {
-    static native void send(ByteBuffer buffer);
-    static native ByteBuffer recv();
+public class LinkImpl implements Closeable {
+    private long linkHandle;  // for c++
+
+    static native long openNative(String name);
+    static native void sendNative(long handle, ByteBuffer buffer);
+    static native ByteBuffer recvNative(long handle);
+    static native boolean closeNative(long handle);
+
+    LinkImpl(String name) throws IOException {
+	linkHandle = openNative(name);
+	if (linkHandle == 0) { throw new IOException(); }	    
+    }
+    public void close() throws IOException {
+	if(linkHandle != 0) {
+	    if(!closeNative(linkHandle)) { throw new IOException(); }
+	}
+    }
+    public void send(ByteBuffer buffer) {
+	sendNative(linkHandle, buffer);
+    }
+    public ByteBuffer recv() {
+	return recvNative(linkHandle);
+    }
 }
--- modules/sql/src/main/native/src/LinkImpl.cpp
+++ modules/sql/src/main/native/src/LinkImpl.cpp
@@ -1,15 +1,61 @@
+#include <iostream>
+
 #include <jni.h>
 #include "com_nautilus_technologies_tsubakuro_impl_low_sql_LinkImpl.h"
 
-JNIEXPORT void JNICALL Java_com_nautilus_1technologies_tsubakuro_impl_low_sql_LinkImpl_send
-  ([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jobject thisObj, [[maybe_unused]] jobject buffer)
+class server {
+public:
+    server([[maybe_unused]] std::string_view name) {
+        std::cout << __func__ << ":" << __LINE__ << ": name : " << name << std::endl;
+    }
+    void send(std::string_view data) {
+        std::cout << __func__ << ":" << __LINE__ << ": Buffer : " <<
+            static_cast<const void*>(data.data()) << " : " << data.length()<< std::endl;
+    }
+    std::string recv() {
+        std::string data;
+        data.resize(128);
+        return data;
+    }
+};
+
+JNIEXPORT jlong JNICALL Java_com_nautilus_1technologies_tsubakuro_impl_low_sql_LinkImpl_openNative
+(JNIEnv *env, [[maybe_unused]] jclass thisObj, jstring name)
 {
-//    jclass cls = env->GetObjectClass(buffer);
-//    char *buf = (char*)env->GetDirectBufferAddress(buffer);
-//    jlong capacity = env->GetDirectBufferCapacity(buffer);
+    const char* name_ = env->GetStringUTFChars(name, NULL);
+    if (name_ == NULL) return 0;
+    jsize len_ = env->GetStringUTFLength(name);
+    server* server_ = new server(std::string_view(name_, len_));
+                                
+    return static_cast<jlong>(reinterpret_cast<std::uintptr_t>(server_));
 }
 
-JNIEXPORT void JNICALL Java_com_nautilus_1technologies_tsubakuro_impl_low_sql_LinkImpl_recv
-  ([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jobject thisObj, [[maybe_unused]] jobject buffer)
+JNIEXPORT void JNICALL Java_com_nautilus_1technologies_tsubakuro_impl_low_sql_LinkImpl_sendNative
+(JNIEnv *env, [[maybe_unused]] jclass thisObj, jlong handle, jobject buffer)
 {
+    server* server_ = reinterpret_cast<server*>(static_cast<std::uintptr_t>(handle));
+
+    char *buf = (char*)env->GetDirectBufferAddress(buffer);
+    jlong capacity = env->GetDirectBufferCapacity(buffer);
+
+    server_->send(std::string_view(buf, capacity));
+}
+
+
+JNIEXPORT jobject JNICALL Java_com_nautilus_1technologies_tsubakuro_impl_low_sql_LinkImpl_recvNative
+(JNIEnv *env, [[maybe_unused]] jclass thisObj, jlong handle)
+{
+    server* server_ = reinterpret_cast<server*>(static_cast<std::uintptr_t>(handle));
+
+    auto data = server_->recv();
+    return env->NewDirectByteBuffer(data.data(), data.length());
+}
+
+JNIEXPORT jboolean JNICALL Java_com_nautilus_1technologies_tsubakuro_impl_low_sql_LinkImpl_closeNative
+([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass thisObj, jlong handle)
+{
+    server* server_ = reinterpret_cast<server*>(static_cast<std::uintptr_t>(handle));
+
+    delete server_;
+    return static_cast<jboolean>(true);
 }
