26a210f3e7615499554c65bd7cc587882bd9129c
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-06-08 22:18:34 +0900

    made ResultSetWireMock an internal class of ResultSetImplTest and number of records to be read increased to two

--- modules/sql/src/test/java/com/nautilus_technologies/tsubakuro/impl/low/sql/ResultSetImplTest.java
+++ modules/sql/src/test/java/com/nautilus_technologies/tsubakuro/impl/low/sql/ResultSetImplTest.java
@@ -5,7 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageUnpacker;
+import org.msgpack.core.MessageFormat;
+import org.msgpack.value.ValueType;
+
 import com.nautilus_technologies.tsubakuro.low.sql.ResultSet;
 import com.nautilus_technologies.tsubakuro.low.sql.SchemaProtos;
 import com.nautilus_technologies.tsubakuro.low.sql.CommonProtos;
@@ -14,43 +21,139 @@ import com.nautilus_technologies.tsubakuro.low.sql.ProtosForTest;
 import org.junit.jupiter.api.Test;
 
 class ResultSetImplTest {
+    class ResultSetWireMock implements ResultSetWire {
+	class MsgPackInputStreamMock extends MsgPackInputStream {
+	    private ByteBuffer buf;
+	    private int position;
+	    
+	    MsgPackInputStreamMock() {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		MessagePacker packer = org.msgpack.core.MessagePack.newDefaultPacker(outputStream);
+
+		try {
+		    // first column data
+		    packer.packLong((long) 987654321);
+		    packer.packDouble((double) 12345.6789);
+		    packer.packString("This is a string for the test");
+		    packer.packLong((long) 123456789);
+		    packer.packDouble((double) 98765.4321);
+		    packer.packNil();
+
+		    // second column data
+		    packer.packLong((long) 876543219);
+		    packer.packDouble((double) 2345.67891);
+		    packer.packNil();
+		    packer.packLong((long) 234567891);
+		    packer.packDouble((double) 8765.43219);
+		    packer.packString("This is second string for the test");
+
+		    packer.flush();
+		} catch (IOException e) {
+		    System.out.println("error");
+		}
+		byte[] ba = outputStream.toByteArray();
+		var size = outputStream.size();
+
+		buf = ByteBuffer.allocateDirect(size);
+		buf.put(ba, 0, size);
+		buf.rewind();
+		position = 0;
+	    }
+
+	    public synchronized int read() throws IOException {
+		if (!buf.hasRemaining()) {
+		    return -1;
+		}
+		return buf.get();
+	    }
+	    public synchronized int read(byte[] bytes, int off, int len) throws IOException {
+		if (!buf.hasRemaining()) {
+		    return -1;
+		}
+		len = Math.min(len, buf.remaining());
+		buf.get(bytes, off, len);
+		return len;
+	    }
+	    public void dispose(long length) {
+		position += length;
+		buf.position(position);
+	    }
+	}
+
+	MsgPackInputStreamMock msgPackInputStreamMock;
+
+	ResultSetWireMock() {
+	    msgPackInputStreamMock = new MsgPackInputStreamMock();
+	}
+
+	public SchemaProtos.RecordMeta recvMeta() throws IOException {
+	    return ProtosForTest.SchemaProtosChecker.builder().build();
+	}
+
+	public MsgPackInputStream getMsgPackInputStream() {
+	    return msgPackInputStreamMock;
+	}
+
+	public void close() throws IOException {
+	}
+    }
+
     private ResultSetImpl resultSetImpl;
-    private ResultSet.RecordMeta recordMeta;
 
     @Test
     void receiveSchemaMetaAndRecord() {
+	ResultSet.RecordMeta recordMeta;
         try {
 	    resultSetImpl = new ResultSetImpl(new ResultSetWireMock());
 
 	    recordMeta = resultSetImpl.getRecordMeta();
-	    assertTrue(recordMeta.fieldCount() == 6);
-	    assertTrue(recordMeta.at(0) == CommonProtos.DataType.INT8);
-	    assertTrue(recordMeta.nullable(0) == false);
-	    assertTrue(recordMeta.at(1) == CommonProtos.DataType.FLOAT8);
-	    assertTrue(recordMeta.nullable(1) == false);
-	    assertTrue(recordMeta.at(2) == CommonProtos.DataType.STRING);
-	    assertTrue(recordMeta.nullable(2) == true);
-	    assertTrue(recordMeta.at(3) == CommonProtos.DataType.INT8);
-	    assertTrue(recordMeta.nullable(3) == false);
-	    assertTrue(recordMeta.at(4) == CommonProtos.DataType.FLOAT8);
-	    assertTrue(recordMeta.nullable(4) == false);
-	    assertTrue(recordMeta.at(5) == CommonProtos.DataType.STRING);
-	    assertTrue(recordMeta.nullable(5) == true);
+	    assertEquals(recordMeta.fieldCount(), 6);
+	    assertEquals(recordMeta.at(0), CommonProtos.DataType.INT8);
+	    assertFalse(recordMeta.nullable(0));
+	    assertEquals(recordMeta.at(1), CommonProtos.DataType.FLOAT8);
+	    assertFalse(recordMeta.nullable(1));
+	    assertEquals(recordMeta.at(2), CommonProtos.DataType.STRING);
+	    assertTrue(recordMeta.nullable(2));
+	    assertEquals(recordMeta.at(3), CommonProtos.DataType.INT8);
+	    assertFalse(recordMeta.nullable(3));
+	    assertEquals(recordMeta.at(4), CommonProtos.DataType.FLOAT8);
+	    assertFalse(recordMeta.nullable(4));
+	    assertEquals(recordMeta.at(5), CommonProtos.DataType.STRING);
+	    assertTrue(recordMeta.nullable(5));
 
+	    // first column data
 	    assertTrue(resultSetImpl.nextRecord());
 	    assertTrue(resultSetImpl.nextColumn());
-	    assertTrue(resultSetImpl.getInt8() == 987654321L);
+	    assertEquals(resultSetImpl.getInt8(), 987654321L);
 	    assertTrue(resultSetImpl.nextColumn());
-	    assertTrue(resultSetImpl.getFloat8() == (double) 12345.6789);
+	    assertEquals(resultSetImpl.getFloat8(), (double) 12345.6789);
 	    assertTrue(resultSetImpl.nextColumn());
-	    assertTrue(resultSetImpl.getCharacter().equals("This is a string for the test"));
+	    assertEquals(resultSetImpl.getCharacter(), "This is a string for the test");
 	    assertTrue(resultSetImpl.nextColumn());
-	    assertTrue(resultSetImpl.getInt8() == (long) 123456789L);
+	    assertEquals(resultSetImpl.getInt8(), (long) 123456789L);
 	    assertTrue(resultSetImpl.nextColumn());
-	    assertTrue(resultSetImpl.getFloat8() == (double) 98765.4321);
+	    assertEquals(resultSetImpl.getFloat8(), (double) 98765.4321);
 	    assertTrue(resultSetImpl.nextColumn());
 	    assertTrue(resultSetImpl.isNull());
 	    assertFalse(resultSetImpl.nextColumn());
+
+	    // second column data
+	    assertTrue(resultSetImpl.nextRecord());
+	    assertTrue(resultSetImpl.nextColumn());
+	    assertEquals(resultSetImpl.getInt8(), 876543219L);
+	    assertTrue(resultSetImpl.nextColumn());
+	    assertEquals(resultSetImpl.getFloat8(), (double) 2345.67891);
+	    assertTrue(resultSetImpl.nextColumn());
+	    assertTrue(resultSetImpl.isNull());
+	    assertTrue(resultSetImpl.nextColumn());
+	    assertEquals(resultSetImpl.getInt8(), (long) 234567891L);
+	    assertTrue(resultSetImpl.nextColumn());
+	    assertEquals(resultSetImpl.getFloat8(), (double) 8765.43219);
+	    assertTrue(resultSetImpl.nextColumn());
+	    assertEquals(resultSetImpl.getCharacter(), "This is second string for the test");
+	    assertFalse(resultSetImpl.nextColumn());
+
+	    // end of records
 	    assertFalse(resultSetImpl.nextRecord());
 	    
 	} catch (IOException e) {
