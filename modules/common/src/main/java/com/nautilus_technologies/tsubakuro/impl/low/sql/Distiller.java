107ab081ed68c236993ae6238093853f8d4594a2
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-06-14 18:00:38 +0900

    revert a commit titled put each Distiller class in a separate file

--- modules/common/src/main/java/com/nautilus_technologies/tsubakuro/impl/low/sql/Distiller.java
+++ modules/common/src/main/java/com/nautilus_technologies/tsubakuro/impl/low/sql/Distiller.java
@@ -8,3 +8,39 @@ import com.nautilus_technologies.tsubakuro.low.sql.ResponseProtos;
 public interface Distiller<V> {
     V distill(ResponseProtos.Response response);
 }
+
+/**
+ * Distiller class responsible for taking ResponseProtos.Prepare from ResponseProtos.Response.
+ */
+class PrepareDistiller implements Distiller<ResponseProtos.Prepare> {
+    public ResponseProtos.Prepare distill(ResponseProtos.Response response) {
+	return response.getPrepare();
+    }
+}
+
+/**
+ * Distiller class responsible for taking ResponseProtos.ResultOnly from ResponseProtos.Response.
+ */
+class ResultOnlyDistiller implements Distiller<ResponseProtos.ResultOnly> {
+    public ResponseProtos.ResultOnly distill(ResponseProtos.Response response) {
+	return response.getResultOnly();
+    }
+}
+
+/**
+ * Distiller class responsible for taking ResponseProtos.ExecuteQuery from ResponseProtos.Response.
+ */
+class ExecuteQueryDistiller implements Distiller<ResponseProtos.ExecuteQuery> {
+    public ResponseProtos.ExecuteQuery distill(ResponseProtos.Response response) {
+	return response.getExecuteQuery();
+    }
+}
+
+/**
+ * Distiller class responsible for taking ResponseProtos.Begin from ResponseProtos.Response.
+ */
+class BeginDistiller implements Distiller<ResponseProtos.Begin> {
+    public ResponseProtos.Begin distill(ResponseProtos.Response response) {
+	return response.getBegin();
+    }
+}
