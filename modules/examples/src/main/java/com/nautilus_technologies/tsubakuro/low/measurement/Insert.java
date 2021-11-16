a9e8d8f4872e010aa676fc7eeea2252d46a58cd2
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-11-16 16:34:27 +0900

    add a measurement example

---
+++ modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/low/measurement/Insert.java
@@ -0,0 +1,113 @@
+package com.nautilus_technologies.tsubakuro.low.measurement;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Objects;
+import com.nautilus_technologies.tsubakuro.util.Pair;
+import com.nautilus_technologies.tsubakuro.low.connection.Connector;
+import com.nautilus_technologies.tsubakuro.low.sql.Session;
+import com.nautilus_technologies.tsubakuro.low.sql.Transaction;
+import com.nautilus_technologies.tsubakuro.low.sql.ResultSet;
+import com.nautilus_technologies.tsubakuro.low.sql.PreparedStatement;
+import com.nautilus_technologies.tsubakuro.protos.RequestProtos;
+import com.nautilus_technologies.tsubakuro.protos.ResponseProtos;
+import com.nautilus_technologies.tsubakuro.protos.CommonProtos;
+
+public class Insert extends Thread {
+    CyclicBarrier barrier;
+    AtomicBoolean stop;
+    Session session;
+    Transaction transaction;
+    RandomGenerator randomGenerator;
+    Profile profile;
+
+    PreparedStatement prepared5;
+    long paramsWid;
+    long paramsDid;
+    long oid;
+    
+    public Insert(Connector connector, Session session, Profile profile, CyclicBarrier barrier, AtomicBoolean stop) throws IOException, ExecutionException, InterruptedException {
+        this.barrier = barrier;
+        this.stop = stop;
+        this.profile = profile;
+        this.session = session;
+        this.session.connect(connector.connect().get());
+        this.randomGenerator = new RandomGenerator();
+	this.oid = Scale.ORDERS + 1;
+	prepare();
+    }
+
+    void prepare() throws IOException, ExecutionException, InterruptedException {
+        String sql5 = "INSERT INTO NEW_ORDER (no_o_id, no_d_id, no_w_id)VALUES (:no_o_id, :no_d_id, :no_w_id)";
+        var ph5 = RequestProtos.PlaceHolder.newBuilder()
+            .addVariables(RequestProtos.PlaceHolder.Variable.newBuilder().setName("no_o_id").setType(CommonProtos.DataType.INT8))
+            .addVariables(RequestProtos.PlaceHolder.Variable.newBuilder().setName("no_d_id").setType(CommonProtos.DataType.INT8))
+            .addVariables(RequestProtos.PlaceHolder.Variable.newBuilder().setName("no_w_id").setType(CommonProtos.DataType.INT8));
+        prepared5 = session.prepare(sql5, ph5).get();
+    }
+
+    void setParams() {
+	paramsWid = randomGenerator.uniformWithin(1, profile.warehouses);
+        paramsDid = randomGenerator.uniformWithin(1, Scale.DISTRICTS);  // scale::districts
+    }
+
+    public void run() {
+	try {
+	    barrier.await();
+	    
+            long start = System.nanoTime();
+	    long prev = 0;
+	    long now = 0;
+	    while (!stop.get()) {
+		if (Objects.isNull(transaction)) {
+		    transaction = session.createTransaction().get();
+		}
+		setParams();
+		now = System.nanoTime();
+		if (prev != 0) {
+		    profile.commit += (now - prev);
+		}
+		prev = now;
+
+		// INSERT INTO NEW_ORDER (no_o_id, no_d_id, no_w_id)VALUES (:no_o_id, :no_d_id, :no_w_id
+		var ps5 = RequestProtos.ParameterSet.newBuilder()
+		    .addParameters(RequestProtos.ParameterSet.Parameter.newBuilder().setName("no_o_id").setInt8Value(oid++))
+		    .addParameters(RequestProtos.ParameterSet.Parameter.newBuilder().setName("no_d_id").setInt8Value(paramsDid))
+		    .addParameters(RequestProtos.ParameterSet.Parameter.newBuilder().setName("no_w_id").setInt8Value(paramsWid));
+		var future5 = transaction.executeStatement(prepared5, ps5);
+		var result5 = future5.get();
+		if (!ResponseProtos.ResultOnly.ResultCase.SUCCESS.equals(result5.getResultCase())) {
+		    if (ResponseProtos.ResultOnly.ResultCase.ERROR.equals(transaction.rollback().get().getResultCase())) {
+			throw new IOException("error in rollback");
+		    }
+		    transaction = null;
+		    continue;
+		}
+		now = System.nanoTime();
+		profile.body += (now - prev);
+		prev = now;
+
+		profile.count++;
+		if ((profile.count % 1000) == 0) {
+		    transaction.commit().get();
+		    transaction = null;
+		}
+	    }
+            profile.elapsed = System.nanoTime() - start;
+	    
+
+        } catch (IOException | ExecutionException | InterruptedException | BrokenBarrierException e) {
+            System.out.println(e);
+	} finally {
+	    try {
+		prepared5.close();
+		session.close();
+	    } catch (IOException e) {
+		System.out.println(e);
+	    }
+	}
+    }
+}
---
+++ modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/low/measurement/Profile.java
@@ -0,0 +1,41 @@
+package com.nautilus_technologies.tsubakuro.low.measurement;
+
+public class Profile {
+    public long warehouses;
+    public long count;
+    public long elapsed;
+    public long commit;
+    public long head;
+    public long body;
+
+    public Profile(long warehouses) {
+	this.warehouses = warehouses;
+	this.count = 0;
+	this.elapsed = 0;
+	this.commit = 0;
+	this.head = 0;
+	this.body = 0;
+    }
+    long ns2us(long t) {
+	return (t + 500) / 1000;
+    }
+    long div(long a, long b) {
+	if (b == 0) {
+	    return a;
+	}
+	return a / b;
+    }
+    public void print() {
+	System.out.println("====");
+	System.out.printf("%d / %d = %d\n", ns2us(elapsed), count, ns2us(elapsed / count));
+	if (head > 0) {
+	    System.out.printf("%d %d %d / %d = %d %d %d\n", ns2us(head), ns2us(body), ns2us(commit),
+			      count,
+			      ns2us(head / count), ns2us(body / count), ns2us(commit / count));
+	} else {
+	    System.out.printf("%d %d / %d = %d %d\n", ns2us(body), ns2us(commit),
+			      count,
+			      ns2us(body / count), ns2us(commit / count));
+	}
+    }
+}
