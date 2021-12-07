7d63e6be9225952dbedee905b3463d4cbecd70c1
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-12-07 19:34:42 +0900

    added a client program that requests multiple insert operations concurrently

---
+++ modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/low/concurrent/Insert.java
@@ -0,0 +1,84 @@
+package com.nautilus_technologies.tsubakuro.low.concurrent;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+import java.util.Objects;
+import com.nautilus_technologies.tsubakuro.util.Pair;
+import com.nautilus_technologies.tsubakuro.low.connection.Connector;
+import com.nautilus_technologies.tsubakuro.low.sql.Session;
+import com.nautilus_technologies.tsubakuro.low.sql.Transaction;
+import com.nautilus_technologies.tsubakuro.low.sql.ResultSet;
+import com.nautilus_technologies.tsubakuro.low.sql.PreparedStatement;
+import com.nautilus_technologies.tsubakuro.protos.RequestProtos;
+import com.nautilus_technologies.tsubakuro.protos.ResponseProtos;
+import com.nautilus_technologies.tsubakuro.protos.CommonProtos;
+
+public class Insert extends Thread {
+    Session session;
+    Transaction transaction;
+    PreparedStatement prepared5;
+
+    long paramsWid = 1;
+    long paramsDid = 1;
+    long firstOid;
+    int concurrency;
+    
+    public Insert(Connector connector, Session session, int concurrency, long firstOid) throws IOException, ExecutionException, InterruptedException {
+	this.concurrency = concurrency;
+	this.firstOid = firstOid;
+        this.session = session;
+        this.session.connect(connector.connect().get());
+	prepare();
+    }
+
+    void prepare() throws IOException, ExecutionException, InterruptedException {
+        String sql5 = "INSERT INTO NEW_ORDER (no_o_id, no_d_id, no_w_id)VALUES (:no_o_id, :no_d_id, :no_w_id)";
+        var ph5 = RequestProtos.PlaceHolder.newBuilder()
+            .addVariables(RequestProtos.PlaceHolder.Variable.newBuilder().setName("no_o_id").setType(CommonProtos.DataType.INT8))
+            .addVariables(RequestProtos.PlaceHolder.Variable.newBuilder().setName("no_d_id").setType(CommonProtos.DataType.INT8))
+            .addVariables(RequestProtos.PlaceHolder.Variable.newBuilder().setName("no_w_id").setType(CommonProtos.DataType.INT8));
+        prepared5 = session.prepare(sql5, ph5).get();
+    }
+
+    public void run() {
+	List<Future<ResponseProtos.ResultOnly>> futures = new ArrayList<>();
+	
+	try {
+	    if (Objects.isNull(transaction)) {
+		transaction = session.createTransaction().get();
+	    }
+
+	    long oid = firstOid + 1;
+	    for (int i = 0; i < concurrency; i++) {
+		// INSERT INTO NEW_ORDER (no_o_id, no_d_id, no_w_id)VALUES (:no_o_id, :no_d_id, :no_w_id
+		var ps5 = RequestProtos.ParameterSet.newBuilder()
+		    .addParameters(RequestProtos.ParameterSet.Parameter.newBuilder().setName("no_o_id").setInt8Value(oid++))
+		    .addParameters(RequestProtos.ParameterSet.Parameter.newBuilder().setName("no_d_id").setInt8Value(paramsDid))
+		    .addParameters(RequestProtos.ParameterSet.Parameter.newBuilder().setName("no_w_id").setInt8Value(paramsWid));
+		futures.add(transaction.executeStatement(prepared5, ps5));
+	    }
+	    for (int i = 0; i < concurrency; i++) {
+		var result5 = futures.get(i).get();
+		if (!ResponseProtos.ResultOnly.ResultCase.SUCCESS.equals(result5.getResultCase())) {
+		    throw new IOException("error in sql");
+		}
+	    }
+	    var commitResponse = transaction.commit().get();
+	    if (!ResponseProtos.ResultOnly.ResultCase.SUCCESS.equals(commitResponse.getResultCase())) {
+		throw new IOException("commit (insert) error");
+	    }
+        } catch (IOException | ExecutionException | InterruptedException e) {
+            System.out.println(e);
+	} finally {
+	    try {
+		prepared5.close();
+		session.close();
+	    } catch (IOException e) {
+		System.out.println(e);
+	    }
+	}
+    }
+}
---
+++ modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/low/concurrent/Main.java
@@ -0,0 +1,77 @@
+package com.nautilus_technologies.tsubakuro.low.concurrent;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.Objects;
+import com.nautilus_technologies.tsubakuro.impl.low.connection.IpcConnectorImpl;
+import com.nautilus_technologies.tsubakuro.impl.low.sql.SessionImpl;
+import com.nautilus_technologies.tsubakuro.protos.ResponseProtos;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.ParseException;
+
+public final class Main {
+    static long orderId()  throws IOException, ExecutionException, InterruptedException {
+        var connector = new IpcConnectorImpl(dbName);
+        var session = new SessionImpl();
+        session.connect(connector.connect().get());
+
+        var transaction = session.createTransaction().get();
+        var future = transaction.executeQuery("SELECT no_o_id FROM NEW_ORDER WHERE no_w_id = 1 AND no_d_id = 1 ORDER by no_o_id DESC");
+        var resultSet = future.getLeft().get();
+        long count = 0;
+        if (resultSet.nextRecord()) {
+            if (resultSet.nextColumn()) {
+                count = resultSet.getInt8();
+            }
+        }
+        resultSet.close();
+	var r = future.getRight().get();
+        if (!ResponseProtos.ResultOnly.ResultCase.SUCCESS.equals(r.getResultCase())) {
+            throw new IOException("select error");
+        }
+	var commitResponse = transaction.commit().get();
+	if (!ResponseProtos.ResultOnly.ResultCase.SUCCESS.equals(commitResponse.getResultCase())) {
+	    throw new IOException("commit (select) error");
+	}
+        session.close();
+        return count;
+    }
+
+    private Main() {
+    }
+    
+    private static String dbName = "tateyama";
+    private static int concurrency = 1;
+    
+    public static void main(String[] args) {
+	// コマンドラインオプションの設定
+        Options options = new Options();
+
+        options.addOption(Option.builder("c").argName("concurrency").hasArg().desc("concurrnency level").build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+
+	    if (cmd.hasOption("c")) {
+		concurrency = Integer.parseInt(cmd.getOptionValue("c"));
+	    }
+
+	    var client = new Insert(new IpcConnectorImpl(dbName), new SessionImpl(), concurrency, orderId());
+	    if (!Objects.isNull(client)) {
+		client.start();
+		client.join();
+	    }
+        } catch (IOException | ExecutionException | InterruptedException e) {
+            System.out.println(e);
+        } catch (ParseException e) {
+            System.err.printf("cmd parser failed." + e);
+        }
+    }
+}
