5764ee428c7bb8a40514e960bc133b79e3301019
Takashi Horikawa <horikawa@nautilus-technologies.com>
2022-02-01 18:28:48 +0900

    add a skeleton of tpc-h benchmark client

---
+++ modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/low/tpch/Main.java
@@ -0,0 +1,75 @@
+package com.nautilus_technologies.tsubakuro.low.tpch;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import com.nautilus_technologies.tsubakuro.impl.low.connection.IpcConnectorImpl;
+import com.nautilus_technologies.tsubakuro.impl.low.sql.SessionImpl;
+import com.nautilus_technologies.tsubakuro.protos.ResponseProtos;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.ParseException;
+
+public final class Main {
+    static long scale()  throws IOException, ExecutionException, InterruptedException {
+	var connector = new IpcConnectorImpl(dbName);
+	var session = new SessionImpl();
+	session.connect(connector.connect().get());
+
+	var transaction = session.createTransaction().get();
+	var future = transaction.executeQuery("SELECT COUNT(S_SUPPKEY) FROM SUPPLIER");
+	var resultSet = future.getLeft().get();
+	long count = 0;
+	if (resultSet.nextRecord()) {
+	    if (resultSet.nextColumn()) {
+		count = resultSet.getInt8();
+	    }
+	}
+	resultSet.close();
+	if (!ResponseProtos.ResultOnly.ResultCase.SUCCESS.equals(future.getRight().get().getResultCase())) {
+	    throw new IOException("select error");
+	}
+	transaction.commit().get();
+	session.close();
+	return count / 10000;
+    }
+
+    private Main() {
+    }
+
+    static String dbName = "tateyama";
+
+    public static void main(String[] args) {
+        // コマンドラインオプションの設定
+        Options options = new Options();
+
+        options.addOption(Option.builder("l").argName("loop").hasArg().desc("Number of loops.").build());
+        options.addOption(Option.builder("q").argName("query_number").hasArg().desc("Query number to be executed.").build());
+        options.addOption(Option.builder("e").argName("query_string").hasArg().desc("Execute this query.").build());
+        options.addOption(Option.builder("f").argName("file_contains_query").hasArg().desc("Execute the query descrived in the file specified.").build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("q")) {
+		System.out.println(cmd.getOptionValue("q"));
+            }
+
+	    var scales = scale();
+	    System.out.println("benchmark started, scale = " + scales);
+	} catch (IOException | ExecutionException | InterruptedException e) {
+	    System.out.println(e);
+        } catch (ParseException e) {
+            System.err.printf("cmd parser failed." + e);
+        }
+    }
+}
