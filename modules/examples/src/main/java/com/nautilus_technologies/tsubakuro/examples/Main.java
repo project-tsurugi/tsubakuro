8edca42a6a2ff41e11e42e2bf176bc3782747b14
Takashi Horikawa <horikawa@nautilus-technologies.com>
2022-06-20 12:26:38 +0900

    add -s (select only) option to examples/Main.java

--- modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/examples/Main.java
+++ modules/examples/src/main/java/com/nautilus_technologies/tsubakuro/examples/Main.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.nautilus_technologies.tsubakuro.channel.common.connection.UsernamePasswordCredential;
 import com.nautilus_technologies.tsubakuro.low.common.Session;
 import com.nautilus_technologies.tsubakuro.low.common.SessionBuilder;
@@ -11,19 +18,41 @@ import com.nautilus_technologies.tsubakuro.exception.ServerException;
 import com.nautilus_technologies.tsubakuro.low.sql.SqlClient;
 
 public final class Main {
-    private Main() {
+    private Main(String[] args) {
     }
 
     private static String url = "ipc:tateyama";
+    private static boolean selectOnly = false;
 
     public static void main(String[] args) {
+        // コマンドラインオプションの設定
+        Options options = new Options();
+
+        options.addOption(Option.builder("s").argName("select").desc("Select only mode.").build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("s")) {
+                System.err.println("select only");
+                selectOnly = true;
+            }
+        } catch (ParseException e) {
+            System.err.println("cmd parser failed." + e);
+        }
+
         try (
             Session session = SessionBuilder.connect(url)
             .withCredential(new UsernamePasswordCredential("user", "pass"))
             .create(10, TimeUnit.SECONDS);
             SqlClient sqlClient = SqlClient.attach(session);) {
 
-            (new Insert(sqlClient)).prepareAndInsert();
+            if (!selectOnly) {
+                (new Insert(sqlClient)).prepareAndInsert();
+            }
             (new Select(sqlClient)).prepareAndSelect();
         } catch (IOException | ServerException | InterruptedException | TimeoutException e) {
             System.out.println(e);
