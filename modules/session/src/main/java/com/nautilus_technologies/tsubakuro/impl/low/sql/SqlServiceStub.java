15fa9bdf4cf4c60a5a0a0b7f91d94cbf77a1f189
Takashi Horikawa <horikawa@nautilus-technologies.com>
2022-06-22 20:09:16 +0900

    ensure one ChannelResponse for one SqlResponse

--- modules/session/src/main/java/com/nautilus_technologies/tsubakuro/impl/low/sql/SqlServiceStub.java
+++ modules/session/src/main/java/com/nautilus_technologies/tsubakuro/impl/low/sql/SqlServiceStub.java
@@ -33,6 +33,7 @@ import com.nautilus_technologies.tsubakuro.channel.common.wire.Response;
 import com.nautilus_technologies.tsubakuro.channel.common.wire.ResponseProcessor;
 import com.nautilus_technologies.tsubakuro.util.Owner;
 import com.nautilus_technologies.tsubakuro.channel.common.ForegroundFutureResponse;
+import com.nautilus_technologies.tsubakuro.channel.common.ChannelResponse;
 
 /**
  * An interface to communicate with SQL service.
@@ -376,9 +377,10 @@ public class SqlServiceStub implements SqlService {
             var payload = response.waitForMainResponse();
             var sqlResponse = SqlResponse.Response.parseDelimitedFrom(new ByteBufferInputStream(payload));
 
-            var futureResponse = FutureResponse.wrap(Owner.of(response));
+            var channelResponse = new ChannelResponse(wire);
+            channelResponse.setResponseHandle(response.responseWireHandle());
+            var futureResponse = FutureResponse.wrap(Owner.of(channelResponse));
             var future = new ForegroundFutureResponse<SqlResponse.ResultOnly>(futureResponse, new SecondResponseProcessor().asResponseProcessor());
-
             if (SqlResponse.Response.ResponseCase.EXECUTE_QUERY.equals(sqlResponse.getResponseCase())) {
                 var detailResponse = sqlResponse.getExecuteQuery();
                 resultSetImpl.connect(detailResponse.getName(), detailResponse.getRecordMeta(), future);
