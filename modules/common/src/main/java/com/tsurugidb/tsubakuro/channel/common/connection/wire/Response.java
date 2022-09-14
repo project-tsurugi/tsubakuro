1f4cf15a40dc797698c83b81fbc1f69b5bd2d7ae
Takashi Horikawa <horikawa@nautilus-technologies.com>
2022-09-14 18:56:03 +0900

    revise stream architecture for receiving response

--- modules/common/src/main/java/com/tsurugidb/tsubakuro/channel/common/connection/wire/Response.java
+++ modules/common/src/main/java/com/tsurugidb/tsubakuro/channel/common/connection/wire/Response.java
@@ -48,27 +48,37 @@ public interface Response extends ServerResource {
     ByteBuffer waitForMainResponse(long timeout, TimeUnit unit)
             throws IOException, ServerException, InterruptedException, TimeoutException;
 
-                /**
-     * Returns the main response body.
-     * If the main response body is not ready, this operation was blocked until it would be ready.
-     * @return ByteBuffer of the main response body
-     * @throws IOException if I/O error was occurred while retrieving main response body
-     * @throws ServerException if server error was occurred while retrieving main response body
-     * @throws InterruptedException if interrupted while retrieving main response body
+    /**
+     * Returns whether or not the sub response body is already available.
+     * That is, {@link #waitForSecondResponse()} returns the sub response data without blocking.
+     * Behavior is undefined if this response is already {@link Response#close() closed}.
+     * @return {@code true} if the sub response is already available, otherwise {@code false}
+     */
+    default boolean isSecondResponseReady() {
+        return false;
+    }
+
+    /**
+     * Returns the sub response body.
+     * If the sub response body is not ready, this operation was blocked until it would be ready.
+     * @return ByteBuffer of the sub response body
+     * @throws IOException if I/O error was occurred while retrieving sub response body
+     * @throws ServerException if server error was occurred while retrieving sub response body
+     * @throws InterruptedException if interrupted while retrieving sub response body
      */
     default ByteBuffer waitForSecondResponse() throws IOException, ServerException, InterruptedException {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Returns the main response body.
-     * If the main response body is not ready, this operation was blocked until it would be ready.
+     * Returns the sub response body.
+     * If the sub response body is not ready, this operation was blocked until it would be ready.
      * @param timeout the maximum time to wait
      * @param unit the time unit of {@code timeout}
-     * @return ByteBuffer of the main response body
-     * @throws IOException if I/O error was occurred while retrieving main response body
-     * @throws ServerException if server error was occurred while retrieving main response body
-     * @throws InterruptedException if interrupted while retrieving main response body
+     * @return ByteBuffer of the sub response body
+     * @throws IOException if I/O error was occurred while retrieving sub response body
+     * @throws ServerException if server error was occurred while retrieving sub response body
+     * @throws InterruptedException if interrupted while retrieving sub response body
      * @throws TimeoutException if the wait time out;
      *      please attention that this exception may occur shorter time than the {@code timeout}
      */
