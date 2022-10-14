7afd2b4766baf43ac870861914f5957495cfa239
Takashi Horikawa <horikawa@nautilus-technologies.com>
2022-10-14 15:29:16 +0900

    revise examples/tpcc/Client.java

--- modules/examples/src/main/java/com/tsurugidb/tsubakuro/examples/tpcc/Client.java
+++ modules/examples/src/main/java/com/tsurugidb/tsubakuro/examples/tpcc/Client.java
@@ -44,83 +44,77 @@ public class  Client extends Thread {
     }
 
     public void run() {
-    int pendingDelivery = 0;
-    int wId;
+        int pendingDelivery = 0;
+        int wId;
 
-    try (
-        Session session = SessionBuilder.connect(url)
-        .withCredential(new UsernamePasswordCredential("user", "pass"))
-        .create(10, TimeUnit.SECONDS);
-        SqlClient sqlClient = SqlClient.attach(session);) {
+        try (Session session = SessionBuilder.connect(url)
+                .withCredential(new UsernamePasswordCredential("user", "pass"))
+                .create(10, TimeUnit.SECONDS);) {
+            try (SqlClient sqlClient = SqlClient.attach(session);) {
+                newOrder = new NewOrder(sqlClient, randomGenerator, profile);
+                payment = new Payment(sqlClient, randomGenerator, profile);
+                delivery = new Delivery(sqlClient, randomGenerator, profile);
+                orderStatus = new OrderStatus(sqlClient, randomGenerator, profile);
+                stockLevel = new StockLevel(sqlClient, randomGenerator, profile);
+                prepare();
 
-        newOrder = new NewOrder(sqlClient, randomGenerator, profile);
-        payment = new Payment(sqlClient, randomGenerator, profile);
-        delivery = new Delivery(sqlClient, randomGenerator, profile);
-        orderStatus = new OrderStatus(sqlClient, randomGenerator, profile);
-        stockLevel = new StockLevel(sqlClient, randomGenerator, profile);
-        prepare();
+                barrier.await();
+                long start = System.currentTimeMillis();
 
-        barrier.await();
-        long start = System.currentTimeMillis();
+                while (!stop.get()) {
+                    long transactionStart = System.nanoTime();
+                    if (pendingDelivery > 0) {
+                        wId = (int) delivery.warehouseId();
+                        if (!doingDelivery.getAndSet(wId - 1, true)) {
+                            delivery.transaction(stop);
+                            profile.time.delivery += (System.nanoTime() - transactionStart);
+                            doingDelivery.set(wId - 1, false);
+                            pendingDelivery--;
+                            if (pendingDelivery > 0) {
+                                delivery.setParams();
+                            }
+                            continue;
+                        }
+                    }
 
-        while (!stop.get()) {
-        long transactionStart = System.nanoTime();
-        if (pendingDelivery > 0) {
-            wId = (int) delivery.warehouseId();
-            if (!doingDelivery.getAndSet(wId - 1, true)) {
-            delivery.transaction(stop);
-            profile.time.delivery += (System.nanoTime() - transactionStart);
-            doingDelivery.set(wId - 1, false);
-            pendingDelivery--;
-            if (pendingDelivery > 0) {
-                delivery.setParams();
+                    var transactionType = randomGenerator.uniformWithin(1, 100);
+                    if (transactionType <= Percent.KXCT_NEWORDER_PERCENT) {
+                        newOrder.setParams();
+                        newOrder.transaction(stop);
+                        profile.time.newOrder += (System.nanoTime() - transactionStart);
+                    } else if (transactionType <= Percent.KXCT_PAYMENT_PERCENT) {
+                        payment.setParams();
+                        payment.transaction(stop);
+                        profile.time.payment += (System.nanoTime() - transactionStart);
+                    } else if (transactionType <= Percent.KXCT_ORDERSTATUS_PERCENT) {
+                        orderStatus.setParams();
+                        orderStatus.transaction(stop);
+                        profile.time.orderStatus += (System.nanoTime() - transactionStart);
+                    } else if (transactionType <= Percent.KXCT_DELIEVERY_PERCENT) {
+                        if (pendingDelivery > 0) {
+                            pendingDelivery++;
+                            continue;
+                        }
+                        delivery.setParams();
+                        wId = (int) delivery.warehouseId();
+                        if (!doingDelivery.getAndSet(wId - 1, true)) {
+                            delivery.transaction(stop);
+                            profile.time.delivery += (System.nanoTime() - transactionStart);
+                            doingDelivery.set(wId - 1, false);
+                        } else {
+                            pendingDelivery++;
+                        }
+                    } else {
+                        stockLevel.setParams();
+                        stockLevel.transaction(stop);
+                        profile.time.stockLevel += (System.nanoTime() - transactionStart);
+                    }
+                }
+                profile.elapsed = System.currentTimeMillis() - start;
             }
-            continue;
-            }
-        }
-
-        var transactionType = randomGenerator.uniformWithin(1, 100);
-        if (transactionType <= Percent.KXCT_NEWORDER_PERCENT) {
-            newOrder.setParams();
-            newOrder.transaction(stop);
-            profile.time.newOrder += (System.nanoTime() - transactionStart);
-        } else if (transactionType <= Percent.KXCT_PAYMENT_PERCENT) {
-            payment.setParams();
-            payment.transaction(stop);
-            profile.time.payment += (System.nanoTime() - transactionStart);
-        } else if (transactionType <= Percent.KXCT_ORDERSTATUS_PERCENT) {
-            orderStatus.setParams();
-            orderStatus.transaction(stop);
-            profile.time.orderStatus += (System.nanoTime() - transactionStart);
-        } else if (transactionType <= Percent.KXCT_DELIEVERY_PERCENT) {
-            if (pendingDelivery > 0) {
-            pendingDelivery++;
-            continue;
-            }
-            delivery.setParams();
-            wId = (int) delivery.warehouseId();
-            if (!doingDelivery.getAndSet(wId - 1, true)) {
-            delivery.transaction(stop);
-            profile.time.delivery += (System.nanoTime() - transactionStart);
-            doingDelivery.set(wId - 1, false);
-            } else {
-            pendingDelivery++;
-            }
-        } else {
-            stockLevel.setParams();
-            stockLevel.transaction(stop);
-            profile.time.stockLevel += (System.nanoTime() - transactionStart);
-        }
-        }
-        profile.elapsed = System.currentTimeMillis() - start;
-        try {
-        session.close();
-        } catch (IOException e) {
-        System.out.println(e);
-        }
-    } catch (IOException | ServerException | InterruptedException | BrokenBarrierException | TimeoutException e) {
-        System.out.println(e);
-        e.printStackTrace();
+        } catch (IOException | ServerException | InterruptedException | BrokenBarrierException | TimeoutException e) {
+            System.err.println(e);
+            e.printStackTrace();
         }
     }
 }
