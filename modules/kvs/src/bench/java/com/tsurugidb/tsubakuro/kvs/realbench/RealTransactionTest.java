9f4f0eb75f731bf9fda2a81aad056e5e1171e233
Katsuhiko Ohkubo <ohkubo@nautilus-technologies.com>
2023-06-21 15:36:00 +0900

    fix for real connection test

--- modules/kvs/src/bench/java/com/tsurugidb/tsubakuro/kvs/realbench/RealTransactionTest.java
+++ modules/kvs/src/bench/java/com/tsurugidb/tsubakuro/kvs/realbench/RealTransactionTest.java
@@ -1,7 +1,6 @@
 package com.tsurugidb.tsubakuro.kvs.realbench;
 
 import java.net.URI;
-import java.text.MessageFormat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import com.tsurugidb.tsubakuro.channel.common.connection.Credential;
 import com.tsurugidb.tsubakuro.channel.common.connection.NullCredential;
 import com.tsurugidb.tsubakuro.common.SessionBuilder;
+import com.tsurugidb.tsubakuro.kvs.GetResult;
 import com.tsurugidb.tsubakuro.kvs.KvsClient;
 import com.tsurugidb.tsubakuro.kvs.bench.RecordBuilder;
 import com.tsurugidb.tsubakuro.kvs.bench.RecordInfo;
@@ -25,12 +25,9 @@ public class RealTransactionTest {
     private final Credential credential = NullCredential.INSTANCE;
 
     RealTransactionTest(String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException(
-                    MessageFormat.format("usage: java {0} <connection-uri>", RealTransactionTest.class.getName()));
-        }
-        LOG.debug("endpoint: {}", args[0]); //$NON-NLS-1$
-        this.endpoint = URI.create(args[0]);
+        String name = (args.length > 0 ? args[0] : "ipc:tsurugi");
+        LOG.debug("endpoint: {}", name); //$NON-NLS-1$
+        this.endpoint = URI.create(name);
     }
 
     private void test() throws Exception {
@@ -38,15 +35,19 @@ public class RealTransactionTest {
         final String table = "TABLE1";
         try (var session = SessionBuilder.connect(endpoint).withCredential(credential).create();
             var kvs = KvsClient.attach(session); var tx = kvs.beginTransaction().await()) {
-            int n = kvs.get(tx, table, builder.makeRecordBuffer()).await().asList().size();
-            System.err.println(n);
-            n = kvs.put(tx, table, builder.makeRecordBuffer()).await().size();
-            System.err.println(n);
-            n = kvs.get(tx, table, builder.makeRecordBuffer()).await().size();
+            int n = kvs.put(tx, table, builder.makeRecordBuffer()).await().size();
             System.err.println(n);
             n = kvs.remove(tx, table, builder.makeRecordBuffer()).await().size();
             System.err.println(n);
+            GetResult get = kvs.get(tx, table, builder.makeRecordBuffer()).await();
+            System.err.println(get.size());
+            for (var rec : get.asList()) {
+                for (int i = 0; i < rec.size(); i++) {
+                    System.out.println(i + "\t" + rec.getName(i) + "\t" + rec.getValue(i));
+                }
+            }
             kvs.commit(tx).await();
+            System.err.println("commit");
         }
     }
 
