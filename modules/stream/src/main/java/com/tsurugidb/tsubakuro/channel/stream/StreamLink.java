89b129d6888431cb57e1a2dd75129ffeaa0bde5d
t-horikawa <horikawa@nautilus-technologies.com>
2023-07-25 16:14:29 +0900

    treate timeout values less than 0 as 0

--- modules/stream/src/main/java/com/tsurugidb/tsubakuro/channel/stream/StreamLink.java
+++ modules/stream/src/main/java/com/tsurugidb/tsubakuro/channel/stream/StreamLink.java
@@ -84,7 +84,7 @@ public final class StreamLink extends Link {
         LinkMessage message = null;
         boolean intentionalClose = true;
         try {
-            socket.setSoTimeout(timeout == 0 ? 0 : (int) unit.toMillis(timeout));
+            socket.setSoTimeout(timeout > 0 ? (int) unit.toMillis(timeout) : 0);
         } catch (SocketException e) {
             socketError.set(true);
         }
