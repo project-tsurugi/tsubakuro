889a364a7d7e41f06e1707579295996c1818d121
Katsuhiko Ohkubo <ohkubo@nautilus-technologies.com>
2023-09-11 15:45:35 +0900

    add Commit type tests

--- modules/kvs/src/inttest/java/com/tsurugidb/tsubakuro/kvs/basic/CommitTest.java
+++ modules/kvs/src/inttest/java/com/tsurugidb/tsubakuro/kvs/basic/CommitTest.java
@@ -1,17 +1,19 @@
 package com.tsurugidb.tsubakuro.kvs.basic;
 
-import java.util.LinkedList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import com.tsurugidb.tsubakuro.kvs.CommitType;
 import com.tsurugidb.tsubakuro.kvs.KvsClient;
+import com.tsurugidb.tsubakuro.kvs.KvsServiceCode;
+import com.tsurugidb.tsubakuro.kvs.KvsServiceException;
 import com.tsurugidb.tsubakuro.kvs.PutType;
 import com.tsurugidb.tsubakuro.kvs.RecordBuffer;
-import com.tsurugidb.tsubakuro.kvs.TransactionOption;
 import com.tsurugidb.tsubakuro.kvs.util.TestBase;
 
-public class CommitTest extends TestBase {
+class CommitTest extends TestBase {
 
     private static final String TABLE_NAME = "table" + CommitTest.class.getSimpleName();
     private static final String KEY_NAME = "k1";
@@ -23,15 +25,35 @@ public class CommitTest extends TestBase {
     }
 
     @Test
+    public void unspecified() throws Exception {
+        try (var session = getNewSession(); var kvs = KvsClient.attach(session)) {
+            try (var tx = kvs.beginTransaction().await()) {
+                RecordBuffer buffer = new RecordBuffer();
+                buffer.add(KEY_NAME, 1L);
+                buffer.add(VALUE_NAME, 100L);
+                kvs.put(tx, TABLE_NAME, buffer, PutType.OVERWRITE).await();
+                kvs.commit(tx, CommitType.UNSPECIFIED).await();
+            }
+        }
+    }
+
+    @Test
     public void commitTypes() throws Exception {
         try (var session = getNewSession(); var kvs = KvsClient.attach(session)) {
             for (var cmtType : CommitType.values()) {
+                if (cmtType == CommitType.UNSPECIFIED) {
+                    continue;
+                }
                 try (var tx = kvs.beginTransaction().await()) {
                     RecordBuffer buffer = new RecordBuffer();
                     buffer.add(KEY_NAME, 1L);
                     buffer.add(VALUE_NAME, 100L);
                     kvs.put(tx, TABLE_NAME, buffer, PutType.OVERWRITE).await();
-                    kvs.commit(tx, cmtType).await();
+                    KvsServiceException ex = assertThrows(KvsServiceException.class, () -> {
+                        kvs.commit(tx, cmtType).await();
+                    });
+                    assertEquals(KvsServiceCode.NOT_IMPLEMENTED, ex.getDiagnosticCode());
+                    kvs.rollback(tx);
                 }
             }
         }
