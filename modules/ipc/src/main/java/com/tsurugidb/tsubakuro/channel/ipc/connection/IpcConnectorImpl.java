70710081bd1b9c98acc87cf5dc1ac27180c44f66
t-horikawa <horikawa@nautilus-technologies.com>
2023-10-20 12:35:55 +0900

    instead of making connectors each time, connectors are now used all the time

--- modules/ipc/src/main/java/com/tsurugidb/tsubakuro/channel/ipc/connection/IpcConnectorImpl.java
+++ modules/ipc/src/main/java/com/tsurugidb/tsubakuro/channel/ipc/connection/IpcConnectorImpl.java
@@ -3,6 +3,7 @@ package com.tsurugidb.tsubakuro.channel.ipc.connection;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.lang.ref.Cleaner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +31,10 @@ public final class IpcConnectorImpl implements Connector {
     private static native void closeConnectorNative(long handle);
 
     private final String name;
+    private final Cleaner cleaner = Cleaner.create();
+    private final CloseRunnable closeRunnable;
+    private final Cleaner.Cleanable cleanable;
+    private long handle;
 
     static {
         NativeLibrary.load();
@@ -37,38 +42,50 @@ public final class IpcConnectorImpl implements Connector {
 
     public IpcConnectorImpl(String name) {
         this.name = name;
+
+        // for GC
+        this.closeRunnable = new CloseRunnable();
+        this.cleanable = cleaner.register(this, closeRunnable);
     }
 
     @Override
     public FutureResponse<Wire> connect(Credential credential) throws IOException {
         LOG.trace("will connect to {}", name); //$NON-NLS-1$
 
-        long handle = getConnectorNative(name);
+        if (handle == 0) {
+            handle = getConnectorNative(name);
+        }
         try {
             long id = requestNative(handle);
-            return new FutureIpcWireImpl(this, handle, id);
+            return new FutureIpcWireImpl(this, id);
         } catch (IOException e) {
             throw new IOException("the server has declined the connection request");
         }
     }
 
-    public Wire getSessionWire(long handle, long id) throws IOException {
+    public Wire getSessionWire(long id) throws IOException {
         long sessionId = waitNative(handle, id);
-        closeConnectorNative(handle);
         return new WireImpl(new IpcLink(name + "-" + String.valueOf(sessionId)), sessionId);
     }
 
-    public Wire getSessionWire(long handle, long id, long timeout, TimeUnit unit) throws TimeoutException, IOException {
+    public Wire getSessionWire(long id, long timeout, TimeUnit unit) throws TimeoutException, IOException {
         var timeoutNano = unit.toNanos(timeout);
         if (timeoutNano == Long.MIN_VALUE) {
             throw new IOException("timeout duration overflow");
         }
         long sessionId = waitNative(handle, id, timeoutNano);
-        closeConnectorNative(handle);
         return new WireImpl(new IpcLink(name + "-" + String.valueOf(sessionId)), sessionId);
     }
 
-    boolean checkConnection(long handle, long id) {
+    boolean checkConnection(long id) {
         return checkNative(handle, id);
+    }
+
+    private class CloseRunnable implements Runnable {
+        CloseRunnable() {
+        }
+        public void run() {
+            closeConnectorNative(handle);
+        }
     }
 }
