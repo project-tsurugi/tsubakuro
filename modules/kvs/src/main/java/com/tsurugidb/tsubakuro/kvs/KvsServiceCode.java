892cc65f3d4c8ebbce7b4517753ba3d8358bc61c
Katsuhiko Ohkubo <ohkubo@nautilus-technologies.com>
2024-02-15 11:01:21 +0900

    docs: add annotation to kvs diagnostic code enums.

--- modules/kvs/src/main/java/com/tsurugidb/tsubakuro/kvs/KvsServiceCode.java
+++ modules/kvs/src/main/java/com/tsurugidb/tsubakuro/kvs/KvsServiceCode.java
@@ -5,55 +5,68 @@ import java.util.HashMap;
 import javax.annotation.Nonnegative;
 
 import com.tsurugidb.tsubakuro.exception.DiagnosticCode;
+import com.tsurugidb.tsubakuro.util.Doc;
 
 /**
  * Code of KVS service diagnostics.
  */
+@Doc(
+        value = "KVS (Key Value Store) service is designed to access the database directly without using SQL.",
+        note = "This feature is able to use partially, but still under development.")
 public enum KvsServiceCode implements DiagnosticCode {
 
     /**
      * the target element does not exist.
      */
+    @Doc("the target element does not exist in the database.")
     NOT_FOUND(1),
 
     /**
      * the target element already exists.
      */
+    @Doc("the target element attempted to create already exists in the database.")
     ALREADY_EXISTS(2),
 
     /**
-     * transaction operation is rollbacked by user.
+     * the transaction operation was rollbacked by user.
      */
+    @Doc("the requested transaction operation was rollbacked by user.")
     USER_ROLLBACK(3),
 
     /**
-     * the operation is waiting for other transaction
+     * the transaction operation is waiting for other transaction.
      */
+    @Doc("the requested transaction operation is waiting for other transaction.")
     WAITING_FOR_OTHER_TRANSACTION(4),
 
     /**
      * Unknown error.
      */
+    @Doc("unknown error was occurred in the kvs service.")
     UNKNOWN(100),
 
     /**
      * I/O error.
      */
+    @Doc("I/O error was occurred in the server.")
     IO_ERROR(102),
 
     /**
      * API arguments are invalid.
      */
+    @Doc("the service received a request message with an invalid argument.")
     INVALID_ARGUMENT(103),
 
     /**
      * API state is invalid.
      */
+    @Doc("the operation was requested in illegal or inappropriate time.")
     INVALID_STATE(104),
 
     /**
      * operation is unsupported.
      */
+    @Doc("the requested operation is not supported.")
     UNSUPPORTED(105),
 
     /**
@@ -62,114 +75,140 @@ public enum KvsServiceCode implements DiagnosticCode {
      * this code is returned only from transaction_exec() and transaction_commit()
      * </p>
      */
+    @Doc("the transaction operation met an user-defined error.")
     USER_ERROR(106),
 
     /**
-     * transaction is aborted
+     * transaction was aborted.
      */
+    @Doc("the transaction was aborted.")
     ABORTED(107),
 
     /**
-     * transaction is aborted, but retry might resolve the situation
+     * transaction was aborted, but retry might resolve the situation.
      */
+    @Doc("the transaction was aborted, but retry might resolve the situation.")
     ABORT_RETRYABLE(108),
 
     /**
-     * api call timed out
+     * api call timed out.
      */
+    @Doc("the request was timed out.")
     TIME_OUT(109),
 
     /**
-     * the feature is not yet implemented
+     * the feature is not yet implemented.
      */
+    @Doc("the requested feature is not yet implemented.")
     NOT_IMPLEMENTED(110),
 
     /**
-     * the operation is not valid
+     * the operation is not valid.
      */
+    @Doc("the requested operation is not valid.")
     ILLEGAL_OPERATION(111),
 
     /**
-     * the operation conflicted on write preserve
+     * the operation conflicted with write preserve.
      */
+    @Doc("the requested operation conflicted with a write preserve.")
     CONFLICT_ON_WRITE_PRESERVE(112),
 
     /**
-     * long tx issued write operation without preservation
+     * long tx issued write operation without write preserve.
      */
+    @Doc("long transaction issued write operation without a write preserve.")
     WRITE_WITHOUT_WRITE_PRESERVE(114),
 
     /**
-     * transaction is inactive
+     * transaction is inactive.
      * <p>
-     * transaction is inactive since it's already committed or aborted. The request is failed.
+     * transaction is inactive because it's already committed or aborted. The request is failed.
      * </p>
      */
+    @Doc(
+            value = "the transaction is inactive.",
+            note = "the transaction is inactive because it's already committed or aborted. The request is failed.")
     INACTIVE_TRANSACTION(115),
 
     /**
-     * requested operation is blocked by concurrent operation
+     * requested operation was blocked by concurrent operation.
      * <p>
-     * the request cannot be fulfilled due to the operation concurrently executed by other transaction.
+     * the request couldn't be fulfilled due to the operation concurrently executed by other transaction.
      * After the blocking transaction completes, re-trying the request may lead to different result.
      * </p>
      */
+    @Doc(
+            value = "the requested operation was blocked by concurrent operation.",
+            note = "the request couldn't be fulfilled due to the operation concurrently executed by other transaction."
+                    + " After the blocking transaction completes, re-trying the request may lead to different result.")
     BLOCKED_BY_CONCURRENT_OPERATION(116),
 
     /**
-     * reached resource limit and request could not be accomplished
+     * reached resource limit and request could not be accomplished.
      */
+    @Doc("the server reached resource limit and the request could not be accomplished.")
     RESOURCE_LIMIT_REACHED(117),
 
     /**
-     * key length passed to the API is invalid
+     * key length passed to the API was invalid.
      */
+    @Doc("key length passed to the API was invalid.")
     INVALID_KEY_LENGTH(118),
 
     /**
-     * The operation result data is too large.
+     * operation result data was too large.
      */
+    @Doc("the operation result data was too large.")
     RESULT_TOO_LARGE(1_001),
 
     /**
-     * Target resource is not authorized.
+     * target resource is not authorized.
      */
+    @Doc("target resource is not authorized to use.")
     NOT_AUTHORIZED(2_001),
 
 
     /**
-     * Transaction is aborted by writing out of write preservation, or writing in read only transaction.
+     * transaction was aborted by writing out of write preservation, or writing in read only transaction.
      */
+    @Doc("the transaction was aborted by writing out of write preservation, or writing in read only transaction.")
     WRITE_PROTECTED(12_002),
 
     /**
-     * The specified table is not found.
+     * the specified table was not found.
      */
+    @Doc("the specified table was not found.")
     TABLE_NOT_FOUND(20_001),
 
     /**
-     * The specified column is not found.
+     * the specified column was not found.
      */
+    @Doc("the specified column was not found.")
     COLUMN_NOT_FOUND(20_002),
 
     /**
-     * The column type is inconsistent.
+     * the column type was inconsistent.
      */
+    @Doc("the column type was inconsistent.")
     COLUMN_TYPE_MISMATCH(20_003),
 
     /**
-     * The search key is mismatch for the table or index.
+     * the search key was mismatch for the table or index.
      */
+    @Doc("the search key was mismatch for the table or index.")
     MISMATCH_KEY(20_011),
 
     /**
-     * Several columns are not specified in {@code PUT} operation.
+     * several columns were not specified in {@code PUT} operation.
      */
+    @Doc("several columns were not specified in PUT operation.")
     INCOMPLETE_COLUMNS(20_021),
 
     /**
-     * Operations was failed by integrity constraint violation.
+     * operations was failed by integrity constraint violation.
      */
+    @Doc("operations was failed by integrity constraint violation.")
     INTEGRITY_CONSTRAINT_VIOLATION(30_001),
 
     ;
