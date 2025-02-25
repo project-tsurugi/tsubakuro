53e313193557daa4b842a4e71f71d9fd5f9cd010
t-horikawa <horikawa@nautilus-technologies.com>
2025-02-25 12:27:15 +0900

    LargeObjectCache inherits ServerResource

--- modules/session/src/main/java/com/tsurugidb/tsubakuro/sql/LargeObjectCache.java
+++ modules/session/src/main/java/com/tsurugidb/tsubakuro/sql/LargeObjectCache.java
@@ -18,10 +18,12 @@ package com.tsurugidb.tsubakuro.sql;
 import java.nio.file.Path;
 import java.util.Optional;
 
+import com.tsurugidb.tsubakuro.util.ServerResource;
+
 /**
  * Represents large object cache.
  */
-public interface LargeObjectCache extends AutoCloseable {
+public interface LargeObjectCache extends ServerResource {
 
     /**
      * Returns the path of the file that represents the large object, only if it exists.
@@ -29,10 +31,4 @@ public interface LargeObjectCache extends AutoCloseable {
      * @return the path of the file, or empty if it does not exist
      */
     Optional<Path> find();
-
-    /**
-     * Closes the object cache. The file whose Path has been returned by find() may be deleted.
-     */
-    @Override
-    void close();
 }
