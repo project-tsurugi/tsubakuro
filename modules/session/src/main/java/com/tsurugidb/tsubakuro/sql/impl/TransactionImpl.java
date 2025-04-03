8a409db0ff74c2c007c08946269c2803e3726451
t-horikawa <horikawa@nautilus-technologies.com>
2025-04-03 17:53:49 +0900

    remove trace message output

--- modules/session/src/main/java/com/tsurugidb/tsubakuro/sql/impl/TransactionImpl.java
+++ modules/session/src/main/java/com/tsurugidb/tsubakuro/sql/impl/TransactionImpl.java
@@ -494,7 +494,6 @@ public class TransactionImpl implements Transaction {
                 disposer.add(new Disposer.DelayedClose() {
                     @Override
                     public void delayedClose() throws ServerException, IOException, InterruptedException {
-                        System.out.println("transaction close " + this.toString());
                         doClose();
                     }
                 });
