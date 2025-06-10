a12e3efd8887ab924846edeff6eb31e09018b1f1
t-horikawa <horikawa@nautilus-technologies.com>
2025-06-10 17:29:29 +0900

    apply commnets

--- modules/session/src/main/java/com/tsurugidb/tsubakuro/sql/TableMetadata.java
+++ modules/session/src/main/java/com/tsurugidb/tsubakuro/sql/TableMetadata.java
@@ -55,7 +55,7 @@ public interface TableMetadata extends RelationMetadata {
      * Returns the primary key information of the relation.
      * @return the primaly key column name list
      *
-     * @since 1.9.0
+     * @since 1.10.0
      */
     List<String> getPrimaryKeys();
 }
