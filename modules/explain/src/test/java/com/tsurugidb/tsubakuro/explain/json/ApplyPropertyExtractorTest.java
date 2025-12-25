ba5e18ffde972e755bd67a07b3232ecd39763464
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2025-12-25 09:47:19 +0900

    feat: execution plan reader supports `apply` operator.

---
+++ modules/explain/src/test/java/com/tsurugidb/tsubakuro/explain/json/ApplyPropertyExtractorTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023-2024 Project Tsurugi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tsurugidb.tsubakuro.explain.json;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.tsurugidb.tsubakuro.explain.PlanGraphException;
+import com.tsurugidb.tsubakuro.explain.PlanNode;
+
+class ApplyPropertyExtractorTest {
+
+    private static PlanNode load(String name) throws IOException, PlanGraphException {
+        var plan = TestUtil.loadPlan(name, Map.of("apply", new ApplyPropertyExtractor()));
+        return TestUtil.get(plan, "apply");
+    }
+
+    @Test
+    void apply() throws Exception {
+        var node = load("apply");
+        assertEquals("apply", node.getTitle());
+
+        var attrs = node.getAttributes();
+        assertEquals("cross", attrs.get("apply-type"));
+        assertEquals("tvf", attrs.get("function"));
+    }
+}
